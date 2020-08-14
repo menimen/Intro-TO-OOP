f9f8d67716a7c2942f614cd60c468696392d084d
menimen <menash9288@gmail.com>
2020-08-14 17:38:24 +0300

    fixed bug in Sort class

--- ex1/src/Sort.java
+++ ex1/src/Sort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class Sort {
     public static void main(String[] args) {
         int[] str = DescribeNumbers.stringsToInts(args);
-        bubbleSort(str);
+        bubbleSort1(str);
         sortASC(str);
         sortDESC(str);
     }
@@ -17,16 +17,30 @@ public class Sort {
     }
     public static void sortDESC(int[] str) {
         int i = str.length-1;
-        while(i > 0 ) {
+        while(i >= 0 ) {
             System.out.print(str[i] + " ");
             i--;
+        }
+    }
+    public static void swap(int i , int j, int arr[]){
+        int temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public static void bubbleSort1(int arr[]) {
+        for(int i =0;i<arr.length;i++) {
+            for(int j=0;j<arr.length-i-1;j++) {
+                if(arr[j]>arr[j+1]) {
+                    swap(j,j+1,arr);
+                }
+            }
         }
     }
     public static void bubbleSort(int arr[])
     {
         int n = arr.length;
         for (int i = 0; i < n-1; i++)
-            for (int j = 0; j < n-i-1; j++)
+            for (int j = 0; j <= n-i-1; j++)
                 if (arr[j] > arr[j+1])
                 {
                     // swap arr[j+1] and arr[i]
